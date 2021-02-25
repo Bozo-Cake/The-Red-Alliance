62cf6136aa988452e985b017c69bbe8506596c54
Brody Roelofs <u771368366@byui.edu>
2021-02-24 18:30:43 -0700

    Load File Picker working. Working on Returning file object.

---
+++ app/src/main/java/team/cake/theredalliance/ConfigReader.java
@@ -0,0 +1,11 @@
+package team.cake.theredalliance;
+
+public class ConfigReader {
+    String _file;
+    ConfigReader(String file) {
+        _file = file;
+    }
+    private void start() {
+
+    }
+}
--- app/src/main/java/team/cake/theredalliance/DisplayGroup1Activity.java
+++ app/src/main/java/team/cake/theredalliance/DisplayGroup1Activity.java
@@ -2,13 +2,35 @@ package team.cake.theredalliance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class DisplayGroup1Activity extends AppCompatActivity {
-
+    int FILE_REQUEST = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_group1);
+    }
+
+    public void getConfigFile(View view) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("text/csv");
+        startActivityForResult(intent, FILE_REQUEST);
+        Uri uri1 = intent.getData();
+        if(uri1 == null) {
+            Toast.makeText(getApplicationContext(),"uri is null",Toast.LENGTH_LONG).show();
+        }
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == FILE_REQUEST && resultCode == Activity.RESULT_OK) {
+            Uri uri = data.getData();
+            String filePath = uri.getPath();
+            System.out.println("Please work " + filePath);
+        }
     }
 }
