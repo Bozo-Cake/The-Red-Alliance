ac81b3fa9dc6aadc9a8a365eb6a9586499ea4473
CaptainTeare <60272522+CaptainTeare@users.noreply.github.com>
2021-03-12 18:34:06 -0700

    One last push for you Sam

---
+++ app/src/main/java/team/cake/theredalliance/TeamViewer.java
@@ -0,0 +1,14 @@
+package team.cake.theredalliance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TeamViewer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_team_viewer);
+    }
+}
